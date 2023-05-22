f83721d436a0ff103638c2acada2f601528e419c
Angel Omar Garnica Alvarez <angelg@advancio.com>
2023-05-22 11:41:53 -0600

    config tsconfig and enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  name: "prod",
+  firebase: {
+    config: {
+      apiKey: "AIzaSyB-r_wq575pP70ozeOJNvVhwplfU41IZwc",
+      authDomain: "buildings-app-14673.firebaseapp.com",
+      projectId: "buildings-app-14673",
+      storageBucket: "buildings-app-14673.appspot.com",
+      messagingSenderId: "309496546573",
+      appId: "1:309496546573:web:72fa28dc829ce86fc2dc44"
+    }
+  }
+}
