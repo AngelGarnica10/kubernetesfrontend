47eb27ce0ca84a0f0dd6225304f3fccbf993a412
Angel Omar Garnica Alvarez <angelg@advancio.com>
2023-06-01 17:00:59 -0600

    logic to register

--- src/app/pages/auth/pages/registration/registration.component.ts
+++ src/app/pages/auth/pages/registration/registration.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import * as fromRoot from '@app/store';
+import * as fromUser from '@app/store/user';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  loading$!: Observable<boolean | null>;
+
+  constructor(
+    private store: Store<fromRoot.State>
+  ) {}
+
+  ngOnInit(): void {
+    this.loading$ = this.store.pipe(select ( fromUser.getLoading ));
+  }
 
   registerUser( form: NgForm): void {
+    if( form.valid ) {
+      const userCreateRequest: fromUser.UserCreateRequest = {
+        name: form.value.name,
+        lastname: form.value.lastname,
+        phone: form.value.phone,
+        username: form.value.username,
+        email: form.value.email,
+        password: form.value.password
+      };
 
+      this.store.dispatch( new fromUser.SignUpEmail(userCreateRequest) );
+    }
   }
 
 }
